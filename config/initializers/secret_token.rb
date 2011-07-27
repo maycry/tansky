bb773910c7bf8ac22209a770b0bfb23d6d8da12c
maycry <byorickq@gmail.com>
2011-07-28 00:27:58 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tansky::Application.config.secret_token = '8f024fb8561991a23c680ad5d62fea13b1c90ab6dc0bd6298199bf4b6af66087178685179f5d10f03056c5f80e9cc3fa0855043e01c802f7e5e9845361314c5a'
