ca31683afca31cdb3e623c1ab26228db8ae3ffb6
maycry <byorickq@gmail.com>
2011-09-02 14:29:49 +0300

    Done auto adding new work on main

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
-  def get_narticles(artnumber = 5)
+  def get_narticles(artnumber = 8)
     Article.find_all_by_onmain(true, :order => 'created_at DESC', :limit => artnumber )
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def get_class(article_number, article_count)
+    case article_number
+      when 0: "center"
+      when 1: "left"
+      when article_count-1: "right"
+      else "out"
+    end 
+  end
 end
