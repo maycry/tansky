c060aef68cb5a4678e33281d446ebe35263c24fe
maycry <byorickq@gmail.com>
2011-10-11 22:56:22 +0300

    float blocks

--- public/javascripts/works.js
+++ public/javascripts/works.js
@@ -56,4 +56,68 @@ $(document).ready(function(){
 			$(this).addClass("opened");	
 		}	
     });
+    
+    
+	window.onload = init; 
+	window.onresize = init;
+	// Функция показывающая ширину экрана, прикольно же.
+	function ShowUsScreenWidth(){
+	    var fmt = document.documentElement.clientWidth;
+	    fmt = fmt - (fmt % 10);
+	    document.getElementById('count').innerHTML=fmt+'px';
+	}
+	
+	var g_count=5; // дефолтное кол-во колонок, генерируемое на сервере
+	
+	
+	function init(){
+	    g_result=document.getElementById("mytable");
+	    if(g_result){
+	        g_tds=g_result.getElementsByTagName("td");
+	        window.onresize=resize;
+	        resize();
+	    }
+	}
+	// Считаем кол-во колонок, которое помещается в экран
+	function CountColumns(){
+	    var c=Math.floor(document.documentElement.offsetWidth/(230));
+	    return c>0?c:1;
+	    }
+	    
+	function resize(){
+	    ShowUsScreenWidth(); //обновляем значение ширины экрана, не относится к скрипту
+	    var cols=CountColumns();// кол-во столбцов в таблице
+	    if(g_count==cols){return;}//ничего не делать, если кол-во колонок то же, что и ранее
+	    g_count=cols; 
+	    var table=document.createElement("table");
+	    var tbody=document.createElement("tbody");
+	    var rows=g_tds.length/cols; // считаем кол-во строк в таблице
+	    var width=100/cols; // ширина каждой ячейки в процентах
+	    for(var j=0;j<rows;j++){
+	        var row1=document.createElement("tr");
+	        for(var i=0;i<cols;i++){
+	            if(!g_tds[0]){break;}
+	            g_tds[0].style.width=width+"%";
+	            row1.appendChild(g_tds[0]);
+	        }
+	        tbody.appendChild(row1);
+	    }
+	    table.appendChild(tbody);
+	    table=g_result.appendChild(table);// добавляем новую таблицу
+	    var prev=table.previousSibling;
+	    if(prev!=null)prev.parentNode.removeChild(prev);// удаляем старую таблицу
+	    
+	}
+	
+	
+	window.onresize = CountColumnsAndSetClass;
+	window.onload = CountColumnsAndSetClass;
+	
+	function CountColumnsAndSetClass(){
+	    var c=Math.floor(document.documentElement.offsetWidth/(300));
+	    cc = c>0?c:1;
+	    document.getElementById('floats').className = 'columns' + cc;
+	    }
+
 });
+
